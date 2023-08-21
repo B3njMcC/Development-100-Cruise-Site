0c444d6115d413141184e3c780772cc1714fdded
B3njMcC <231186@virtualwindow.co.za>
2023-08-21 20:31:06 +0200

    Added Filtering
    add filtering for trips page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 let tripArr = [
 {
-    location: 'Hawaii',
-    price: 12000,
-    duration: 3
+    location: 'Lima to Buenos',
+    price: 85439.74,
+    duration: "18 Dec - 11 Jan 2024",
+    picture: "Hawaii.jpg",
+    filter: "expensive"
 },
 {
-    location: 'Malaysia',
-    price: 10000,
-    duration: 4
+    location: 'Miami to Lima',
+    price: 49357.17,
+    duration: "3 Dec - 18 Dec 2023",
+    picture: "",
+    filter: "cheap"
 },
 {
-    location: 'Mauritius',
-    price:  8000,
-    duration: 1
+    location: 'Dubai to Singapore',
+    price:  64549.83,
+    duration: "8 Dec - 28 Dec 2023",
+    filter: "expensive"
+},
+{
+    location: 'Mumbai to Singapore',
+    price:  112026.91,
+    duration: "8 Jan - 28 Jan 2024",
+    picture: "",
+    filter: "expensive"
+},
+{
+    location: 'Bangkok to Hong Kong',
+    price:  91136.99,
+    duration: "25 Jan - 8 Feb 2024",
+    picture: "",
+    filter: "expensive"
 }
 ]
+
+let appliedPriceFilter = "";
 
 //-------------------------
 //When document loads
@@ -29,13 +50,46 @@ $(document).ready(function(){
 //-------------------------
 //Trips Page
 //-------------------------
-    loadTrips();
+    loadTrips(tripArr);
 })
 
 //Load all obj in tripArr
-loadTrips = () => {
-    for(let i = 0; i < tripArr.length; i++){
-        let trips = tripArr[i];
+loadTrips = (tripsToDisplay) => {
+
+    $("#tripsContainer").empty();
+
+    for(let i = 0; i < tripsToDisplay.length; i++){
+        let trips = tripsToDisplay[i];
+        
+        $("#tripsContainer").append($('#tripsTemplate').html());
+
+        let activeChild = $($("#tripsContainer")).children().eq(i);
+
+        $(activeChild).find('#cardTitle').text(trips.location);
+        $(activeChild).find('#cardDuration').text("R" + trips.price);
+        $(activeChild).find('#cardDate').text(trips.duration);
+
         console.log(trips)
     }
+}
+
+//Upon clicking filter radio buttons
+$("input[name='filterTrips']").click(function(){
+    appliedPriceFilter = $(this).attr("value");
+
+
+    filterTrips();
+})
+
+filterTrips = () => {
+
+    let filteredTrips = [];
+
+    if (appliedPriceFilter){
+        filteredTrips = tripArr.filter(trips => trips.filter == appliedPriceFilter);
+    } else {
+        filteredTrips = tripArr;
+    }
+
+    loadTrips(filteredTrips);
 }
